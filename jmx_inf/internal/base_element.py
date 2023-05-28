c5f2616dce7fb72f68e4bb054dfabd18ff918358
shaanNagra <shaan.n@outlook.co.nz>
2023-05-28 17:06:11 +1200

    added Docstring comments to base elements

--- jmx_inf/internal/base_element.py
+++ jmx_inf/internal/base_element.py
@@ -15,7 +15,27 @@ from .constants import HASH_TREE
 #
 # ------------------------------------------
 class BaseElem:
+    """The parent class for creating Jmeter elements.
+
+
+    Attributes:
+        Element: JMX ElementTree.
+        TempRoot: Temporary ElementTree to act as root to Element.
+
+    Methods:
+        append(Element): Appends Element as child to this one.
+        get_elem(): Returns ElementTree Element.
+        print(): Prints JMX of element.
+        enable(enable=True): Sets the enable flag of JMX element.
+        bool_2_bool: Returns string of Boolean value.
+    """
     def __init__(self, element, name):
+        """Constructs parent attributes for JMX Element object.
+        
+        Parameters:
+            element: The string that represent a JMX Element.
+            name: The name of JMX Element.  
+        """
         self.Element = ET.fromstring(element)
         self.Element.set('testname', name)
         self.TempRoot = ET.Element('temproot')
@@ -24,22 +44,41 @@ class BaseElem:
         pass
 
     def append(self, elem):
+        """Appends given element as subelement.
+
+        Parameter:
+            elem: The Element object to append as subelement.
+        """
         self.Element.getnext().extend(elem)
         return
 
     def get_elem(self):
+        """Returns subelements of TempRoot Element.
+
+        Returns:
+            subelements of temporary root.
+        """
         return self.TempRoot.getchildren()
 
     def print(self):
+        """Print string content of element and any sibling elements.
+        """
         print(ET.tostring(self.Element))
         print(ET.tostring(self.Element.getnext()))
         return
 
     def enable(self, enable=True):
+        """Sets enable flag of a JMX Elements.
+        """
         self.Element.set('enabled', self.Bool2Bool(enable))
         return
 
     def bool_2_bool(self, bool):
+        """Returns String of Boolean value.
+
+        Returns:
+            string: "false" or "true"
+        """
         if bool is False:
             return "false"
         elif bool is True:
--- jmx_inf/jmeter_test_script.py
+++ jmx_inf/jmeter_test_script.py
@@ -10,12 +10,21 @@ from jmx_inf.internal.constants import HASH_TREE
 #
 # /////////////////////////////////////////////////
 
-
-
-# ------------------------------------------
-#
-# ------------------------------------------
 class JmeterTestPlan:
+    """The Class that represents the JMeter Element 'Test Plan'.
+
+    Attributes: 
+        version:
+        properties:
+        jmeter:
+
+    Methods:
+        append(Element): Appends Element as child to this one.
+        toString: Returns element content as a string.
+        print(): Prints JMX of element.
+        enable(enable=True): Sets the enable flag of JMX element.
+        bool_2_bool: Returns string of Boolean value.
+    """
     def __init__(self, version, properties, jmeter):
         self.__header = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
 
@@ -27,21 +36,40 @@ class JmeterTestPlan:
         self.Element.append(self.hashTree)
 
     def append(self, elem):
+        """ Appends given element as subelement.
+
+        Parameter:
+            elem: The Element object to append as subelement.
+        """
         self.hashTree.extend(elem)
         return
 
     def print(self):
+        """Print string content of element and any sibling elements.
+        """
         print(ET.tostring(self.Element))
         return
 
     def toString(self):
+        """Returns element content as a string.
+
+        Returns:
+            String: JMX Element content.
+        """
         return ET.tostring(self.Element)
 
     def enable(self, enable=True):
+        """Sets enable flag of a JMX Elements.
+        """
         self.Element.set('enabled', self.Bool2Bool(enable))
         return
 
     def Bool2Bool(self, bool):
+        """Returns String of Boolean value.
+
+        Returns:
+            string: "false" or "true"
+        """
         if bool is False:
             return "false"
         elif bool is True:
